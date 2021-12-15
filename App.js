c60c2523ba7d5142ca8b938d27226aa1d63e86f3
Fe.cardenas <fe.cardenasa@gmail.com>
2021-12-14 22:13:43 -0300

    Primer
    Primer comit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+// redux nunca lo entendi XDD
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={}>
+      <View style={styles.container}>
+        <Text>Vamos hacer un clone de Uber!</Text>
+      </View>
+    </Provider>
+
   );
 }
 
